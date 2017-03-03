faf96c1f542501c3b6397f0ef66829c03264585e
jatinrokad <rokadjatin@gmail.com>
2017-03-04 00:57:07 +0530

    Create connection.php

---
+++ Login-Register/connection.php
@@ -0,0 +1,19 @@
+<?php
+	
+	$con=mysqli_connect("localhost","root","") or die(mysql_error("not found"));
+
+	if($con){
+
+	}
+	else{
+
+	}
+	$db="register_form";
+	if (mysqli_select_db($con,$db)) {
+
+	}
+	else{
+
+	}
+	error_reporting(E_ALL or E_WARNING or E_NOTICE);
+?>
